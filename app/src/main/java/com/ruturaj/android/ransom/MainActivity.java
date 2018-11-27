d7535a1b83a3cdfef466673175b828299eb8c303
Ruturaj <ruturajkvaidya@gmail.com>
2018-11-27 12:31:25 -0600

    Fixes

--- app/src/main/java/com/ruturaj/android/ransom/MainActivity.java
+++ app/src/main/java/com/ruturaj/android/ransom/MainActivity.java
@@ -110,6 +110,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && (grantResults[0] == PackageManager.PERMISSION_GRANTED) && (grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    finish();
+                    startActivity(getIntent());
+                    Toast.makeText (this, "Permissions Granted!", Toast.LENGTH_SHORT).show();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
     public void encrypt (String key, String address, String name) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
 
         File extStore = Environment.getExternalStorageDirectory ();
